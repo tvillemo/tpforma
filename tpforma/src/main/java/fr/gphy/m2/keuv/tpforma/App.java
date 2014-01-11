4a8fd5539d5112d852e3bf6894b107e3aa78fc01
keuv-gravouil <k.gravouil@gmail.com>
2014-01-11 18:21:18 +0100

    hellow world

---
+++ tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/App.java
@@ -0,0 +1,12 @@
+package fr.gphy.m2.keuv.tpforma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
