ad9cb32f5d0de31bf1914a2b030cd04b1bba8ca7
Tanguy <Tanguy@172.16.125.29>
2014-01-14 12:11:25 +0100

    add protviewer+dependencies

---
+++ tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/ProtViewer.java
@@ -0,0 +1,9 @@
+import org.biojava.bio.structure.Structure;
+import org.biojava.bio.structure.gui.BiojavaJmol;
+import org.biojava.bio.structure.io.PDBFilreReader;
+
+public class ProtViewer{
+	ProtViewer(){
+		
+	}
+}
