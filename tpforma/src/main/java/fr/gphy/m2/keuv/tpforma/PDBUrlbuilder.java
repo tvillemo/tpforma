ee55d1dccf67322ef491a5e1cd10c64ef36f2a7a
ameliesimon <Amlie@PC-Mili.wifi.univ-poitiers.fr>
2014-01-14 11:03:05 +0100

    proxy

--- tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/PDBUrlbuilder.java
+++ tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/PDBUrlbuilder.java
@@ -2,6 +2,8 @@
 public class PDBUrlBuilder{
 	public static URL getFirstPdbUrl(String uniprotId) throws IOExcpetion{
 		//gerer proxy
+		System.setProperty("http.proxyHost","cache.univpoitiers.fr");
+		System.setProperty("http.proxyPort","3128");
 		URL url = new URL("http://www.uniprot.org/uniprot/"+uniprotId+".txt");
 		BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 		// lire fichier
